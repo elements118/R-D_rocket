4aeacab4b0a52f25f9b3263350b4568232bce687
elements118 <jacobmarkbell29@gmail.com>
2022-08-02 17:06:37 -0500

    added outer element

--- coax_copy.m
+++ coax_copy.m
@@ -23,7 +23,8 @@ kin_visc_LOX= 2.362 * 10^-6; % [ft^2/s] kinematic viscosity of LOX
 K_guess = 3;
 inner_wall_thck = .005104; % [ft] wall thickness of the inner element, ~ 1/16"
 coeff_nozzle_open = 3; % coefficient of nozzle opening, from Beyvel pg. 263 
-    % should be (2-5), Bazarov says 3x
+    % should be (2-5), Bazarov says 3x. This value is the ratio of "swirl
+    % arm"/nozzle radius
 
 % Initial Calculations
 
@@ -46,13 +47,13 @@ while lcv < 100
     inner_swirl_diam = sqrt(4 * m_dot_LOX / (pi * inner_disc_coeff * ...
         sqrt(2 * LOX_dens * delta_p_LOX * gravity))); % Beyvel, Eq. 5-82
     
-    R = (coeff_nozzle_open * inner_swirl_diam) / 2;
+    inner_R = (coeff_nozzle_open * inner_swirl_diam) / 2; % "swirl arm", center axis to inlet
 
     inner_nozzle_length = 2 * inner_swirl_diam; % Suggested by Bazarov pg. 76
 
-    inner_chamber_length = 3 * R; % From Bazarov, should be (2-3) * R
+    inner_chamber_length = 3 * inner_R; % From Bazarov, should be (2-3) * R
     
-    inner_inlet_diam = sqrt((2 * R * inner_swirl_diam) / (inner_num_inlets * K_guess)); % Beyvel, Eq. 5-84
+    inner_inlet_diam = sqrt((2 * inner_R * inner_swirl_diam) / (inner_num_inlets * K_guess)); % Beyvel, Eq. 5-84
     
     inner_inlet_length = (inner_inlet_diam / 2) * 3; % Suggested by Bazarov to be (3-4) * inlet radius
 
@@ -61,9 +62,9 @@ while lcv < 100
     
     inner_frict_coeff = exp((25.8/log(Re_LOX)^2.58) - 2); % Friction Coefficient Beyvel, Eq. 5-87
     
-    K_frict = (R * (inner_swirl_diam / 2)) / ((inner_num_inlets * ...
+    K_frict = (inner_R * (inner_swirl_diam / 2)) / ((inner_num_inlets * ...
         (inner_inlet_diam / 2)^2) + (inner_frict_coeff / 2) * ...
-        R * (R - (inner_swirl_diam / 2))); % K value taking friction into account Beyvel, Eq. 5-81
+        inner_R * (inner_R - (inner_swirl_diam / 2))); % K value taking friction into account Beyvel, Eq. 5-81
 
     visc_spray_angle = atand((2 * inner_disc_coeff * K_frict) / sqrt((1 + inner_S)^2 - ...
     (4 * inner_disc_coeff^2 * K_guess^2))); % Beyvel, Eq. 5-80
@@ -80,7 +81,7 @@ while lcv < 100
     inner_visc_swirl_diam = sqrt(4 * m_dot_LOX / (pi * inner_visc_disc_coeff * ...
         sqrt(2 * LOX_dens * delta_p_LOX * gravity))); % Beyvel, Eq. 5-82
 
-    K_visc = (2 * R * inner_visc_swirl_diam) / (inner_num_inlets * ...
+    K_visc = (2 * inner_R * inner_visc_swirl_diam) / (inner_num_inlets * ...
         inner_inlet_diam^2); % Beyvel 5-83
 
     lcv = lcv + 1;
@@ -93,6 +94,60 @@ while lcv < 100
         K_guess = K_visc;
     end
 end
+
+% TIME FOR PART TWO!!!!!!
+
+% Inner Element constants and assumptions
+m_dot_FUEL = .5; % [lbm/s] Oxidizer mass flow  
+delta_p_FUEL = 10800; % [lbf/ft^2] Oxidizer pressure drop 
+FUEL_dens = 71.168; % [lb/ft^3] LOX density 
+outer_num_inlets = 3; % [N/A] number of tangential inlets 
+kin_visc_FUEL= 2.362 * 10^-6; % [ft^2/s] kinematic viscosity of LOX
+coeff_nozzle_open = 3; % coefficient of nozzle opening, from Beyvel pg. 263 
+    % should be (2-5), Bazarov says 3x (for closed). This value is the ratio of "swirl
+    % arm"/nozzle radius
+
+% INITIAL VALUES
+permitted_vortex_rad = (external_nozzle_diam / 2) + .00098425; % value comes from bazarov, Pg. 76
+outer_swirl_diam = 2 * permitted_vortex_rad; % first estimation of outer diameter
+effective_flow_area = m_dot_FUEL / sqrt(2 * FUEL_dens * delta_p_FUEL * gravity); % [] Beyvel, Eq. 5-63
+
+outlet_area = (pi / 4) * outer_swirl_diam^2; % [] 
+
+outer_disc_coeff = effective_flow_area / outlet_area; % [N/A]
+
+lcv = 1;
+
+while lcv < 100
+    fcn = @(outer_filling_eff) (outer_filling_eff * sqrt(outer_filling_eff ...
+        / (2 - outer_filling_eff)) - outer_disc_coeff); % Beyvel Eq. 5-66
+
+    outer_filling_eff = fzero(fcn, .7);
+
+    outer_K = (((1 - outer_filling_eff) * sqrt(2)) ...
+        / (outer_filling_eff * sqrt(outer_filling_eff))); % [N/A] Beyvel, Eq. 5-65
+
+    rel_vortex_rad = .1827 * log(7 * outer_K); % from empirical plot
+
+    outer_swirl_diam_new = 2 * (permitted_vortex_rad / rel_vortex_rad); 
+
+    effective_flow_area = m_dot_FUEL / sqrt(2 * FUEL_dens * delta_p_FUEL * gravity); % [] Beyvel, Eq. 5-63
+
+    outlet_area = (pi / 4) * outer_swirl_diam_new^2; % [] 
+
+    outer_disc_coeff = effective_flow_area / outlet_area; % [N/A]
+
+     if abs((outer_swirl_diam_new - outer_swirl_diam) / outer_swirl_diam) < .005
+         break
+     else
+         outer_swirl_diam = outer_swirl_diam_new;
+     end
+     lcv = lcv + 1;
+end
+
+% Outer element final dimensions
+outer_R = (outer_swirl_diam * coeff_nozzle_open) / 2;
+
 
 
 
